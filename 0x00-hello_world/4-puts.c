0b840e17cb192b0874cd4a3869aef6c272a268b5
Princeodi <princewillodinakachukwu@gmail.com>
2022-09-08 13:02:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -17,11 +17,11 @@
  */
 
 int main(void)
-  
+
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
